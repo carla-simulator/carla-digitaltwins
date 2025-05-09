cdfe6e6a9655971318d74342f4ee9355b24f7b25
Marcel Pi <169088301+MarcelPiNacy-CVC@users.noreply.github.com>
2025-05-09 14:14:26 +0200

    Fix some boost build issues through build.cs. Update gitignore.

--- Source/CarlaDigitalTwinsTool/CarlaDigitalTwinsTool.Build.cs
+++ Source/CarlaDigitalTwinsTool/CarlaDigitalTwinsTool.Build.cs
@@ -4,47 +4,150 @@ using UnrealBuildTool;
 using System.IO;
 using System;
 using System.Diagnostics;
+using System.Collections;
+using System.Runtime.InteropServices;
+using EpicGames.Core;
+using System.ComponentModel;
 
 public class CarlaDigitalTwinsTool : ModuleRules
 {
-	private void BuildDependencies()
-	{
-		var Configuration = "";
-		switch (Target.Configuration)
-		{
+	private string GetCMakeConfigurationName()
+  {
+    switch (Target.Configuration)
+    {
       case UnrealTargetConfiguration.Debug:
-				Configuration = "Debug";
-				break;
+        return "Debug";
       case UnrealTargetConfiguration.DebugGame:
-				Configuration = "Debug";
-				break;
+        return "Debug";
       case UnrealTargetConfiguration.Development:
-				Configuration = "RelWithDebInfo";
-				break;
+        return "RelWithDebInfo";
       case UnrealTargetConfiguration.Shipping:
-				Configuration = "Release";
-				break;
+        return "Release";
       case UnrealTargetConfiguration.Test:
-				Configuration = "Test";
-        break;
+        return "Test";
       default:
-				throw new ArgumentException(string.Format("Invalid build configuration \"{0}\"", Target.Configuration));
+        throw new ArgumentException(string.Format("Invalid build configuration \"{0}\"", Target.Configuration));
     }
-		var DepsPath = PluginDirectory;
+  }
+
+	private string GetVCVars() // Windows only.
+  {
+		var Root = Environment.GetEnvironmentVariable("PROGRAMFILES");
+		if (Root == null)
+			throw new DirectoryNotFoundException();
+		Root = Path.Combine(Root, "Microsoft Visual Studio/2022/Community/VC/Auxiliary/Build");
+    if (!Path.Exists(Root))
+      throw new DirectoryNotFoundException(Root);
+		Root = Path.Combine(Root, "vcvars64.bat");
+    if (!Path.Exists(Root))
+      throw new DirectoryNotFoundException(Root);
+		return Root;
+  }
+
+	private string ArrayToCMakeList(string[] elements)
+	{
+		string result = "";
+		if (elements.Length == 0)
+			return result;
+		if (elements.Length == 1)
+			return elements[0];
+		foreach (var element in elements)
+		{
+			result += element;
+      result += ";";
+    }
+		return result;
+	}
+
+  private void BuildBoost()
+	{
+		var BoostComponents = new string[]
+    {
+			"asio",
+			"iterator",
+			"date_time",
+			"geometry",
+			"container",
+			"variant2",
+			"gil",
+    };
+		var BoostComponentsList = ArrayToCMakeList(BoostComponents);
+		var CurrentProcess = Process.GetCurrentProcess();
+    var DepsPath = PluginDirectory;
+		var Configuration = GetCMakeConfigurationName();
+		var BuildPath = Path.Combine(DepsPath, "Build-" + Configuration);
     var PSI = new ProcessStartInfo();
-		PSI.FileName = "cmake";
-		PSI.Arguments = string.Format(" -S {0} -B Build -G Ninja -DCMAKE_BUILD_TYPE={1}", DepsPath, Configuration);
+		bool verbose = true;
+    PSI.WorkingDirectory = DepsPath;
+		if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+		{
+			PSI.FileName = Environment.GetEnvironmentVariable("comspec");
+			PSI.Arguments = string.Format(
+        "/k \"\"{0}\" && cmake -S . -B {1} -G Ninja -DCMAKE_BUILD_TYPE={2} -DBOOST_COMPONENTS={3}\"",
+				GetVCVars(),
+        BuildPath,
+				Configuration,
+        BoostComponentsList);
+			Console.WriteLine(string.Format("Running {0} {1}", PSI.FileName, PSI.Arguments));
+    }
+		else
+		{
+      PSI.FileName = "cmake";
+      PSI.Arguments = string.Format(
+				" -S . -B Build-{0} -G Ninja -DCMAKE_BUILD_TYPE={1} -DBOOST_COMPONENTS={2}",
+				Target.Configuration,
+				Configuration,
+        BoostComponentsList);
+    }
+		
 		var BuildProcess = Process.Start(PSI);
 		BuildProcess.WaitForExit();
 		if (BuildProcess.ExitCode != 0)
-			throw new BuildException("Failed to build module dependencies.");
-	}
+			throw new BuildException(string.Format("Failed to build module dependencies (Exit code = {0}).", BuildProcess.ExitCode));
+
+		var LinkLibrariesPath = Path.Combine(BuildPath, "LinkLibraries.txt");
+		foreach (var Line in File.ReadAllLines(LinkLibrariesPath))
+		{
+			var Trimmed = Line.Trim();
+			if (Trimmed.Length == 0)
+				continue;
+			if (!Path.Exists(Trimmed))
+				throw new FileNotFoundException(Trimmed);
+			if (verbose)
+				Console.WriteLine("Adding " + Trimmed + " to PublicAdditionalLibraries");
+      PublicAdditionalLibraries.Add(Trimmed);
+		}
+
+		var IncludeDirectoriesPath = Path.Combine(BuildPath, "IncludeDirectories.txt");
+    foreach (var Line in File.ReadAllLines(IncludeDirectoriesPath))
+    {
+      var Trimmed = Line.Trim();
+      if (Trimmed.Length == 0)
+        continue;
+      if (!Path.Exists(Trimmed))
+        throw new FileNotFoundException(Trimmed);
+			if (verbose)
+				Console.WriteLine("Adding " + Trimmed + " to PublicIncludePaths");
+      PublicIncludePaths.Add(Trimmed);
+    }
+
+    var PublicDefinitionsPath = Path.Combine(BuildPath, "PublicDefinitions.txt");
+    foreach (var Line in File.ReadAllLines(PublicDefinitionsPath))
+    {
+      var Trimmed = Line.Trim();
+      if (Trimmed.Length == 0)
+        continue;
+			if (verbose)
+				Console.WriteLine("Adding " + Trimmed + " to PublicDefinitions");
+      PublicDefinitions.Add(Trimmed);
+    }
+  }
 
 	public CarlaDigitalTwinsTool(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-
-		BuildDependencies();
+		
+		BuildBoost();
 
 		PublicIncludePaths.AddRange(
 			new string[] {
@@ -89,7 +192,6 @@ public class CarlaDigitalTwinsTool : ModuleRules
 				"FoliageEdit",
 				"MeshMergeUtilities",
 				"StaticMeshDescription",
-				"PhysXVehicles",
 				"Json",
 				"JsonUtilities",
 				"Networking",
@@ -102,6 +204,11 @@ public class CarlaDigitalTwinsTool : ModuleRules
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
+
+		if (Target.Version.MajorVersion < 5)
+		{
+			PrivateDependencyModuleNames.Add("PhysXVehicles");
+		}
 
 
 		DynamicallyLoadedModuleNames.AddRange(
