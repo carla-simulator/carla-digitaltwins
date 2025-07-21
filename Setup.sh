dc910fef6d74d11aaac676c2b630160cc1f46d62
Pablo Villanueva Domingo <pablo.villanueva.domingo@gmail.com>
2025-07-21 11:43:37 +0200

    Update pip install requirements line

--- Setup.sh
+++ Setup.sh
@@ -3,8 +3,6 @@
 SCRIPT_PATH="$(readlink -f -- "${BASH_SOURCE[0]}")"
 SOURCE_DIR="$(dirname "${SCRIPT_PATH}")"
 
-python3 -m pip install -r requirements.txt
-
 BOOST_COMPONENTS="asio;iterator;date_time;geometry;container;variant2;gil;filesystem"
 
 cmake \
@@ -15,3 +13,5 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=OFF \
     -DBOOST_COMPONENTS=${BOOST_COMPONENTS}
+
+python3 -m pip install -r Content/Python/requirements.txt
